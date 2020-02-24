ddbaa356d4fa7524ac8b5d2fe05ccf16dd42cbd0
MarcinBalejko <mbbm9@o2.pl>
2020-02-24 15:04:17 +0100

    :white_check_mark::art: Created chart, typos corrected

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('vote-form');
 
+// Form Submit Event
 form.addEventListener('submit', e => {
   const choice = document.querySelector('input[name=os]:checked').value;
   const data = { os: choice };
@@ -17,3 +18,50 @@ form.addEventListener('submit', e => {
 
   e.preventDefault();
 });
+
+let dataPoints = [
+  { label: 'Windows', y: 0 },
+  { label: 'MacOS', y: 0 },
+  { label: 'Linux', y: 0 },
+  { label: 'Other', y: 0 }
+];
+
+const chartContainer = document.querySelector('#chartContainer');
+
+if (chartContainer) {
+  const chart = new CanvasJS.Chart('chartContainer', {
+    animationEnabled: true,
+    theme: 'theme1',
+    title: {
+      text: 'OS Results'
+    },
+    data: [
+      {
+        type: 'column',
+        dataPoints: dataPoints
+      }
+    ]
+  });
+  chart.render();
+
+  // Enable pusher logging - don't include this in production
+  Pusher.logToConsole = true;
+
+  var pusher = new Pusher('35908522cfbc0d916880', {
+    cluster: 'eu',
+    forceTLS: true
+  });
+
+  var channel = pusher.subscribe('os-poll');
+  channel.bind('os-vote', function(data) {
+    dataPoints = dataPoints.map(x => {
+      if (x.label == data.os) {
+        x.y += data.points;
+        return x;
+      } else {
+        return x;
+      }
+    });
+    chart.render();
+  });
+}
